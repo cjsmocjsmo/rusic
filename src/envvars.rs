7b0a04f8ab5f10f5aca727f8583f1c7cb35e8d04
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-08-08 13:06:41 -0700

    change envars

--- src/envvars.rs
+++ src/envvars.rs
@@ -1,75 +1,23 @@
 use std::env;
 
-// pub fn set_env_vars() {
-//     let usb1 = env::var("RUSIC_USB1");
-//     if usb1.is_err() {
-//         env::set_var("RUSIC_USB1", "/media/pi/FOO/media");
-//     };
-//     let usb2 = env::var("RUSIC_USB2");
-//     if usb2.is_err() {
-//         env::set_var("RUSIC_USB2", "/media/pi/USB2/media");
-//     };
-//     let usb3 = env::var("RUSIC_USB3");
-//     if usb3.is_err() {
-//         env::set_var("RUSIC_USB3", "/media/pi/USB3/media");
-//     };
-//     let usb4 = env::var("RUSIC_USB4");
-//     if usb4.is_err() {
-//         env::set_var("RUSIC_USB4", "None");
-//     };
-
-
-//     let pagination = env::var("RUSIC_PAGINATION");
-//     if pagination.is_err() {
-//         env::set_var("RUSIC_PAGINATION", "25");
-//     };
-//     let rusic = env::var("RUSIC_PATH");
-//     if rusic.is_err() {
-//         env::set_var("RUSIC_PATH", "/usr/share/rusic/rusic");
-//     };
-//     let rusic_thumbs = env::var("RUSIC_THUMBS");
-//     if rusic_thumbs.is_err() {
-//         env::set_var("RUSIC_THUMBS", "/usr/share/rusic/rusic/thumbs");
-//     };
-//     let rusic_nfo = env::var("RUSIC_NFOS");
-//     if rusic_nfo.is_err() {
-//         env::set_var("RUSIC_NFOS", "/usr/share/rusic/rusic/nfo");
-//     };
-//     let rusic_raw_http = env::var("RUSIC_RAW_HTTP");
-//     if rusic_raw_http.is_err() {
-//         env::set_var("RUSIC_RAW_HTTP", "192.168.0.26");
-//     };
-//     let rusic_http = env::var("RUSIC_HTTP_ADDR");
-//     if rusic_http.is_err() {
-//         env::set_var("RUSIC_HTTP_ADDR", "http://192.168.0.26");
-//     };
-//     let rusic_port = env::var("RUSIC_PORT");
-//     if rusic_port.is_err() {
-//         env::set_var("RUSIC_PORT", ":8080");
-//     };
-// }
-
 pub fn set_env_vars() {
     let usb1 = env::var("RUSIC_USB1");
     if usb1.is_err() {
-        env::set_var("RUSIC_USB1", "/media/charliepi/C052-0E64/media/");
+        env::set_var("RUSIC_USB1", "/media/pi/FOO/media");
     };
     let usb2 = env::var("RUSIC_USB2");
     if usb2.is_err() {
-        env::set_var("RUSIC_USB2", "None");
+        env::set_var("RUSIC_USB2", "/media/pi/USB2/media");
     };
     let usb3 = env::var("RUSIC_USB3");
     if usb3.is_err() {
-        env::set_var("RUSIC_USB3", "None");
+        env::set_var("RUSIC_USB3", "/media/pi/USB3/media");
     };
     let usb4 = env::var("RUSIC_USB4");
     if usb4.is_err() {
         env::set_var("RUSIC_USB4", "None");
     };
-    let no_art_pic = env::var("RUSIC_NO_ART_PIC");
-    if no_art_pic.is_err() {
-        env::set_var("RUSIC_NO_ART_PIC", "/media/charliepi/HD/MTVSERVER/rusic/no_art_pic.jpg");
-    };
+
 
     let pagination = env::var("RUSIC_PAGINATION");
     if pagination.is_err() {
@@ -77,29 +25,81 @@ pub fn set_env_vars() {
     };
     let rusic = env::var("RUSIC_PATH");
     if rusic.is_err() {
-        env::set_var("RUSIC_PATH", "/media/charliepi/HD/MTVSERVER/rusic");
+        env::set_var("RUSIC_PATH", "/usr/share/rusic/rusic");
     };
     let rusic_thumbs = env::var("RUSIC_THUMBS");
     if rusic_thumbs.is_err() {
-        env::set_var(
-            "RUSIC_THUMBS",
-            "/media/charliepi/HD/MTVSERVER/rusic/thumbs/",
-        );
+        env::set_var("RUSIC_THUMBS", "/usr/share/rusic/rusic/thumbs");
     };
     let rusic_nfo = env::var("RUSIC_NFOS");
     if rusic_nfo.is_err() {
-        env::set_var("RUSIC_NFOS", "/media/charliepi/HD/MTVSERVER/rusic/nfo");
+        env::set_var("RUSIC_NFOS", "/usr/share/rusic/rusic/nfo");
     };
     let rusic_raw_http = env::var("RUSIC_RAW_HTTP");
     if rusic_raw_http.is_err() {
-        env::set_var("RUSIC_RAW_HTTP", "192.168.0.90");
+        env::set_var("RUSIC_RAW_HTTP", "192.168.0.26");
     };
     let rusic_http = env::var("RUSIC_HTTP_ADDR");
     if rusic_http.is_err() {
-        env::set_var("RUSIC_HTTP_ADDR", "http://192.168.0.90");
+        env::set_var("RUSIC_HTTP_ADDR", "http://192.168.0.26");
     };
     let rusic_port = env::var("RUSIC_PORT");
     if rusic_port.is_err() {
         env::set_var("RUSIC_PORT", ":8080");
     };
 }
+
+// pub fn set_env_vars() {
+//     let usb1 = env::var("RUSIC_USB1");
+//     if usb1.is_err() {
+//         env::set_var("RUSIC_USB1", "/media/charliepi/C052-0E64/media/");
+//     };
+//     let usb2 = env::var("RUSIC_USB2");
+//     if usb2.is_err() {
+//         env::set_var("RUSIC_USB2", "None");
+//     };
+//     let usb3 = env::var("RUSIC_USB3");
+//     if usb3.is_err() {
+//         env::set_var("RUSIC_USB3", "None");
+//     };
+//     let usb4 = env::var("RUSIC_USB4");
+//     if usb4.is_err() {
+//         env::set_var("RUSIC_USB4", "None");
+//     };
+//     let no_art_pic = env::var("RUSIC_NO_ART_PIC");
+//     if no_art_pic.is_err() {
+//         env::set_var("RUSIC_NO_ART_PIC", "/media/charliepi/HD/MTVSERVER/rusic/no_art_pic.jpg");
+//     };
+
+//     let pagination = env::var("RUSIC_PAGINATION");
+//     if pagination.is_err() {
+//         env::set_var("RUSIC_PAGINATION", "25");
+//     };
+//     let rusic = env::var("RUSIC_PATH");
+//     if rusic.is_err() {
+//         env::set_var("RUSIC_PATH", "/media/charliepi/HD/MTVSERVER/rusic");
+//     };
+//     let rusic_thumbs = env::var("RUSIC_THUMBS");
+//     if rusic_thumbs.is_err() {
+//         env::set_var(
+//             "RUSIC_THUMBS",
+//             "/media/charliepi/HD/MTVSERVER/rusic/thumbs/",
+//         );
+//     };
+//     let rusic_nfo = env::var("RUSIC_NFOS");
+//     if rusic_nfo.is_err() {
+//         env::set_var("RUSIC_NFOS", "/media/charliepi/HD/MTVSERVER/rusic/nfo");
+//     };
+//     let rusic_raw_http = env::var("RUSIC_RAW_HTTP");
+//     if rusic_raw_http.is_err() {
+//         env::set_var("RUSIC_RAW_HTTP", "192.168.0.90");
+//     };
+//     let rusic_http = env::var("RUSIC_HTTP_ADDR");
+//     if rusic_http.is_err() {
+//         env::set_var("RUSIC_HTTP_ADDR", "http://192.168.0.90");
+//     };
+//     let rusic_port = env::var("RUSIC_PORT");
+//     if rusic_port.is_err() {
+//         env::set_var("RUSIC_PORT", ":8080");
+//     };
+// }
