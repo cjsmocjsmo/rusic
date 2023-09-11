c2614b624260a71eabe19d1d908741de11157aca
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-09-11 09:53:24 -0700

    update

--- src/server/fragments.rs
+++ src/server/fragments.rs
@@ -7,7 +7,7 @@ pub fn frag_artiscount(xx: Vec<types::ArtistCount>) -> String {
         frag.push_str("<div>");
         frag.push_str("<ul class='alphaList'>");
         frag.push_str("<div class='alpha'>");
-        frag.push_str(&("<a href='/artist/'".to_owned() + x.alpha.as_str() + ">" + x.alpha.as_str() + "</a>"));
+        frag.push_str(&("<a href='/artist/".to_owned() + x.alpha.as_str() + "'>" + x.alpha.as_str() + "</a>"));
         frag.push_str(&("<h1>".to_owned() + x.alpha.as_str() + "</h1>"));
         frag.push_str(&("<h3>".to_owned() + x.count.to_string().as_str() + "</h3>"));
         frag.push_str("</a></div></ul></div>");
