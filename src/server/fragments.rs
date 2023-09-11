68d501dff4580d74b8a8b9799590a33fe815f4e8
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-09-11 10:38:44 -0700

    update

--- src/server/fragments.rs
+++ src/server/fragments.rs
@@ -4,10 +4,8 @@ pub fn frag_artiscount(xx: Vec<types::ArtistCount>) -> String {
     let mut master = Vec::new();
     for x in xx {
         let mut frag = String::new();
-        frag.push_str("<div>");
-        frag.push_str("<ul class='alphaList'>");
-        frag.push_str("<div class='alpha'>");
-        frag.push_str(&("<a href='/artist/".to_owned() + x.alpha.as_str() + "'>" + x.alpha.as_str() + "</a>"));
+        frag.push_str("<div><ul class='alphaList'><div class='alpha'>");
+        frag.push_str(&("<a href='/artist/".to_owned() + x.alpha.as_str() + "'>"));
         frag.push_str(&("<h1>".to_owned() + x.alpha.as_str() + "</h1>"));
         frag.push_str(&("<h3>".to_owned() + x.count.to_string().as_str() + "</h3>"));
         frag.push_str("</a></div></ul></div>");
