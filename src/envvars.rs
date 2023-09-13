e92ac6f69051fe74bf415e8fc29460c425e7eba6
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-09-13 14:00:13 -0700

    serve up music

--- src/envvars.rs
+++ src/envvars.rs
@@ -3,6 +3,7 @@ use std::env;
 pub fn set_env_vars() {
     let usb = env::var("RUSIC_USB");
     if usb.is_err() {
+        // env::set_var("RUSIC_USB", "/media/pi/C052-0E64/Music/K");
         env::set_var("RUSIC_USB", "/media/pi/C052-0E64/Music/K");
     };
 
