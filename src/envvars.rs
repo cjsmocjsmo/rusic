2c61a7c2de95a0524c2ff684111867dbc2a421f1
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-09-17 10:43:04 -0700

    update

--- src/envvars.rs
+++ src/envvars.rs
@@ -4,7 +4,7 @@ pub fn set_env_vars() {
     let usb = env::var("RUSIC_USB");
     if usb.is_err() {
         // env::set_var("RUSIC_USB", "/media/pi/C052-0E64/Music/K");
-        env::set_var("RUSIC_USB", "/media/pi/C052-0E64/Music/K");
+        env::set_var("RUSIC_USB", "/media/pi/C052-0E64/");
     };
 
     let db_path = env::var("RUSIC_DB_PATH");
