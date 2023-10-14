f16d52cb7ef9f6592c48ef27540c726cc5e039e3
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-10-14 10:12:10 -0700

    update

--- src/server.rs
+++ src/server.rs
@@ -42,7 +42,8 @@ pub async fn rusic_server_main() -> std::io::Result<()> {
             .service(fs::Files::new("/Music", "/usr/share/rusic/rusic/Music".to_string()).show_files_listing())
         }
     )
-    .bind(("192.168.0.26", 8080))?
+    // .bind(("192.168.0.26", 8080))?
+    .bind(("192.168.0.91", 8080))?
     .run()
     .await
 }
