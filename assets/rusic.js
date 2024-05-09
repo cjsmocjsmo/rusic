a33b868c8ea6b714267109e2be54e9b8db85950c
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-05-09 10:28:04 -0700

    Refactor albumofinterest route to update PlayMusic function with correct RusicId parameter

--- assets/rusic.js
+++ assets/rusic.js
@@ -3,6 +3,7 @@
 function PlayMusic(event) {
     var musid = event.target.getAttribute('data-musid');
     var url = 'http://192.168.0.97:8080/playmovie?musid=' + musid;
+    
 
     fetch(url)
         .then(response => response.json())
