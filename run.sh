70019d612cd738fec326510977fd2e8874011f21
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-05-28 04:41:25 -0700

    Refactor Dockerfiles to use thumbnails directory for playing.jpg
    The Dockerfiles for the RPI 32 and 64 architectures have been refactored to use the thumbnails directory for the playing.jpg asset. This change ensures that the correct path is used for the thumbnail image in the Rusic application.

--- run.sh
+++ run.sh
@@ -32,13 +32,6 @@ if [ ! -d /usr/share/rusicsetup/rusicsetup ]; then
     exit 1
 fi
 
-# Check if Rust is installed
-# If not, print an error message and exit the script
-if ! command -v rustc &> /dev/null; then
-    echo "Rust is not installed. Please install Rust before running this script."
-    exit 1
-fi
-
 # Check if Golang is installed
 # If not, print an error message and exit the script
 if ! command -v go &> /dev/null; then
@@ -60,36 +53,10 @@ if ! command -v docker &> /dev/null; then
     exit 1
 fi
 
-# Remove old directories and files if they exist
-rm -rf /usr/share/rusic/rusic/nfo;
-rm -rf /usr/share/rusic/rusic/assets/thumbnails;
-rm -rf /usr/share/rusic/rusic/db;
-
-# Create new directories and files
-mkdir /usr/share/rusic/rusic/nfo;
-mkdir /usr/share/rusic/rusic/assets/thumbnails;
-mkdir /usr/share/rusic/rusic/db;
-touch /usr/share/rusic/rusic/db/rusic.db;
-mkdir /usr/share/rusic/rusic/build;
-
-# Print a message indicating that the cleaning process is complete and the build process is starting
-echo "Clean complete\n\nStarting Build\n\n";
-
 # If the first argument is 32, execute the following commands
 if [ "$1" = "32" ]; then
     
-        cd /usr/share/rusic/rusic/build;
-        git pull;
-    
-    
-    # Change to the rusicsetup directory
-    cd rusicsetup;
-    # Build the project
-    cargo build --release;
-    # Copy the built project to the rusic directory
-    cp -pvr target/release/rusicsetup /usr/share/rusic/rusic;
-    # Remove the build directory
-    rm -rf /usr/share/rusic/rusic/build;
+        
     # Copy the Dockerfile for the 32-bit architecture to the current directory
     cp -pvr RPI/32/Dockerfile .
     # Build the Docker image
@@ -105,20 +72,9 @@ if [ "$1" = "32" ]; then
     # Remove the Dockerfile
     rm Dockerfile
     
-# If the first argument is not 32 (i.e., it is 64), execute the following commands
 else
-    # Change to the build directory
-    cd /usr/share/rusic/rusic/build;
-    # Clone the rusicsetup repository
-    git clone https://github.com/cjsmocjsmo/rusicsetup.git;
-    # Change to the rusicsetup directory
-    cd rusicsetup;
-    # Build the project
-    cargo build --release;
-    # Copy the built project to the rusic directory
-    cp -pvr target/release/rusicsetup /usr/share/rusic/rusic;
-    # Remove the build directory
-    rm -rf /usr/share/rusic/rusic/build;
+    
+   
     # Copy the Dockerfile for the 64-bit architecture to the current directory
     cp -pvr RPI/64/Dockerfile .
     # Build the Docker image
