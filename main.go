8694b8e9af5b8077eb22013be20bac25d77b675b
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-06-08 23:04:50 -0700

    `Refactor Dockerfiles to update volume paths and add log file`

--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 package main
 
 import (
--- rusic/rusic.go
+++ rusic/rusic.go
@@ -1,3 +1,9 @@
+// SPDX-FileCopyrightText: 2024 Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+
+
 package rusic
 
 import (
